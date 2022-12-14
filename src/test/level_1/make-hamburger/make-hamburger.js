68d01fc34e256726fcb30b955a1f236a55d2f6e6
jang95 <81616561+jang95@users.noreply.github.com>
2022-12-14 17:51:35 +0900

    hamber success
    other people's solutions

--- src/test/level_1/make-hamburger/make-hamburger.js
+++ src/test/level_1/make-hamburger/make-hamburger.js
@@ -1,32 +1,26 @@
+// 나의 풀이
 function solution(ingredient) {
   let answer = 0;
   const stack = [];
 
-  ingredient.forEach((ing) => {
-    stack.push(ing);
-
+  for (let i = 0; i <= ingredient.length; i++) {
+    const list = stack.push(ingredient[i]); // stack에 ingredient[i]를 넣고 list에 stack의 길이를 저장
     if (stack.length >= 4) {
-      // 길이가 4 이상일 때
-      const find = stack.slice(-4).join(''); // 스택의 마지막 4개를 문자열로 만들어서 find에 저장
+      if (
+        stack[list - 1] === 1 &&
+        stack[list - 2] === 3 &&
+        stack[list - 3] === 2 &&
+        stack[list - 4] === 1
+      ) {
+        stack.pop();
+        stack.pop();
+        stack.pop();
+        stack.pop();
 
-      if (find === '1231') {
-        // find가 1231이면
-        stack.pop();
-        stack.pop();
-        stack.pop();
-        stack.pop();
-        ++answer; // pop 4번하고 answer에 1을 더한다.
+        answer++;
       }
     }
-  });
+  }
 
   return answer;
 }
-
-console.log('1234', solution([2, 1, 1, 2, 3, 1, 2, 3, 1]));
-
-// slice() 메서드는 어떤 배열의 begin부터 end까지 (end는 포함하지 않는) 얕은 복사본을 새로운 배열 객체로 반환합니다. 원본 배열은 바뀌지 않습니다.
-
-// join() 메서드는 배열의 모든 요소를 연결해 하나의 문자열로 만듭니다.
-
-// pop() 메서드는 배열에서 마지막 요소를 제거하고 그 요소를 반환합니다. 이 메서드는 배열의 길이를 변경합니다.
--- src/test/level_1/make-hamburger/make-hamburger3.js
+++ src/test/level_1/make-hamburger/make-hamburger3.js
@@ -1,33 +1,17 @@
 function solution(ingredient) {
-  let answer = 0;
-  const find = '1231';
+  let count = 0;
 
-  const list = ingredient.join('');
-
-  if (list.length >= 4) {
-    if (list.indexOf(find) !== -1) {
-      answer + 1;
+  for (let i = 0; i < ingredient.length; i++) {
+    if (ingredient.slice(i, i + 4).join('') === '1231') {
+      count++;
+      ingredient.splice(i, 4);
+      // for문의 index를 되돌리는 방법
+      i -= 3;
     }
+    console.log(ingredient);
   }
 
-  // if (list.length >= 4) {
-  //   const a = list.indexOf(find);
-  //   console.log(a);
-  // }
-
-  return answer;
+  return count;
 }
 
-console.log(
-  '1234',
-  solution([
-    2, 1, 1, 2, 3, 1, 2, 3, 1, 2, 1, 1, 2, 3, 1, 2, 3, 1, 2, 1, 1, 2, 3, 1, 2, 3, 1, 2, 1, 1, 2, 3,
-    1, 2, 3, 1,
-  ])
-);
-
-// slice() 메서드는 어떤 배열의 begin부터 end까지 (end는 포함하지 않는) 얕은 복사본을 새로운 배열 객체로 반환합니다. 원본 배열은 바뀌지 않습니다.
-
-// join() 메서드는 배열의 모든 요소를 연결해 하나의 문자열로 만듭니다.
-
-// pop() 메서드는 배열에서 마지막 요소를 제거하고 그 요소를 반환합니다. 이 메서드는 배열의 길이를 변경합니다.
+console.log('1234', solution([2, 1, 1, 2, 3, 1, 2, 3, 1]));
