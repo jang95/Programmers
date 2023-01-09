151221c309bf421bd8876bb52847158fc36efe4e
jang95 <81616561+jang95@users.noreply.github.com>
2023-01-09 18:05:41 +0900

    ADD:삼총사 문제
    reduce() 메서드에 대한 학습
    재귀함수 학습 필요

---
+++ src/test/level_1/Three-musketeers/Three2.js
@@ -0,0 +1,24 @@
+function solution(number) {
+  var answer = 0;
+
+  const combination = (current, start) => {
+    if (current.length === 3) {
+      answer += current.reduce((acc, cur) => acc + cur, 0) === 0 ? 1 : 0;
+      return;
+    }
+
+    for (let i = start; i < number.length; i++) {
+      combination([...current, number[i]], i + 1);
+    }
+  };
+  combination([], 0);
+
+  return answer;
+}
+
+/**
+ * 재귀함수를 사용하여 문제 해결
+ * reduce를 사용하여 배열의 합 구하기
+ * acc는 누적값, cur은 현재값이므로 3가지의 값을 더해야 하는 경우 acc에 1번, 2번 값이 들어가고
+ * 3번 값이 cur에 들어가서 3개의 값을 더한 값이 나온다.
+ */
