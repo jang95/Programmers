f529ffa8bcf07eb7a26ffeea3ecb623612a3afa3
jang95 <81616561+jang95@users.noreply.github.com>
2023-01-20 16:08:53 +0900

    숫자 짝궁 문제
    추상화가 잘 된 코드 확인
    Set에 대하여

---
+++ src/test/Level_1/Number-pair/Number-pair3.js
@@ -0,0 +1,32 @@
+// 추상화가 잘 된 코드
+function solution(X, Y) {
+  const commonNumbers = [...new Set(X.split(''))]
+    .filter((number) => {
+      return Y.includes(number);
+    })
+    .sort((a, b) => b - a);
+
+  if (!commonNumbers.length) return '-1';
+
+  if (!Number(commonNumbers[0])) return '0';
+
+  return commonNumbers
+    .map((number) => {
+      const Xcount = X.split('').reduce((count, Xnumber) => {
+        if (Xnumber === number) return (count += 1);
+
+        return count;
+      }, 0);
+
+      const Ycount = Y.split('').reduce((count, Ynumber) => {
+        if (Ynumber === number) return (count += 1);
+
+        return count;
+      }, 0);
+
+      return Xcount <= Ycount ? number.repeat(Xcount) : number.repeat(Ycount);
+    })
+    .join('');
+}
+
+solution('3578935', '3578935');
