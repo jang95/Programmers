c0352a399438d7668059d329f67067c2b9ce1f58
jang95 <81616561+jang95@users.noreply.github.com>
2023-02-01 14:15:52 +0900

    Coding test
    두 수의 합

---
+++ src/Level_0/SumOfTwoNumber/SumOfTwoNumber.js
@@ -0,0 +1,5 @@
+// 나의 풀이
+function solution(num1, num2) {
+  var answer = num1 + num2;
+  return answer;
+}
---
+++ src/Level_0/SumOfTwoNumber/SumOfTwoNumber2.js
@@ -0,0 +1,4 @@
+// 다른 사람 풀이
+
+// 바로 return 형식
+const solution = (num1, num2) => num1 + num2;
