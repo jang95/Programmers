0a8e314d173bc350f18f489c4a70fe73e4e57715
jang95 <81616561+jang95@users.noreply.github.com>
2023-02-22 14:21:09 +0900

    FIX: 주석 내용

--- src/Level_0/문자열 정렬하기/CharSort.js
+++ src/Level_0/문자열 정렬하기/CharSort.js
@@ -1,6 +1,5 @@
 // 나의 풀이
 
-// 문자열에도 index가 존재하므로 split 사용하지 않아도 될 문제...
 function solution(my_string) {
   return my_string
     .replace(/[a-z]/g, '')
@@ -10,7 +9,7 @@ function solution(my_string) {
 }
 
 // 다른 사람 풀이
-// match 사용하여 숫자만 빼는 형식
+// match 사용하여 숫자만으로 배열생성
 function solution(my_string) {
   return my_string
     .match(/\d/g)
