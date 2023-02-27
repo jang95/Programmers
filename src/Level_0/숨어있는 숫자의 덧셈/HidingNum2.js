71fe5974187f9ab7eceaa1585a45e7ae6eebbb8e
jang95 <81616561+jang95@users.noreply.github.com>
2023-02-27 17:02:57 +0900

    숨어있는 숫자의 덧셈(2)

--- src/Level_0/숨어있는 숫자의 덧셈/HidingNum2.js
+++ src/Level_0/숨어있는 숫자의 덧셈/HidingNum2.js
@@ -10,3 +10,15 @@ function solution(my_string) {
 function solution(my_string) {
   return my_string.split(/\D+/).reduce((acc, cur) => acc + Number(cur), 0);
 }
+
+function solution(n) {
+  let count = 0;
+  for (let i = 1; i <= n; i++) {
+    count += 1;
+    if (i % 3 == 0 || i.toString().includes('3')) {
+      console.log('qer', i);
+      count += 1;
+    }
+  }
+  return count;
+}
