08da448042917779287fe1aa2d8cb270a13deaba
jang95 <81616561+jang95@users.noreply.github.com>
2023-03-25 21:20:06 +0900

    짝수와 홀수

---
+++ src/Level_1/짝수와_홀수/짝수와_홀수.js
@@ -0,0 +1,11 @@
+// 나의 풀이
+function solution(num) {
+  return num % 2 === 0 ? 'Even' : 'Odd';
+}
+
+// 다른 사람 풀이
+
+// 0이 false라는 것을 이용한 풀이
+function evenOrOdd(num) {
+  return num % 2 ? 'Odd' : 'Even';
+}
