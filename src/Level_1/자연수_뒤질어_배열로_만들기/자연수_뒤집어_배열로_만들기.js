54b3ee9f938ea49547e59f272f71f5aadec841ac
jang95 <81616561+jang95@users.noreply.github.com>
2023-03-29 17:17:37 +0900

    자연수 뒤집어 배열로 만들기

---
+++ src/Level_1/자연수_뒤질어_배열로_만들기/자연수_뒤집어_배열로_만들기.js
@@ -0,0 +1,22 @@
+// 나의 풀이
+function solution(n) {
+  return ('' + n)
+    .split('')
+    .reverse()
+    .map((x) => +x);
+}
+
+// 다른 사람 풀이
+
+// 숫자풀이
+function solution(n) {
+  // 숫자풀이
+  var arr = [];
+
+  do {
+    arr.push(n % 10);
+    n = Math.floor(n / 10);
+  } while (n > 0);
+
+  return arr;
+}
