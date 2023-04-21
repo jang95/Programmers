ca468c4f99e3061d630d552d6ef1f72cf6bb35e8
jang95 <81616561+jang95@users.noreply.github.com>
2023-04-21 23:24:13 +0900

    최소직사각형

---
+++ src/Level_1/최소직사각형/최소직사각형.js
@@ -0,0 +1,26 @@
+// 나의 문제 풀이
+function solution(sizes) {
+  let w = 0;
+  let h = 0;
+  sizes.forEach((s) => {
+    const [a, b] = s.sort((a, b) => a - b);
+    if (a > h) h = a;
+    if (b > w) w = b;
+  });
+
+  return w * h;
+}
+
+// 다른 사람 풀이
+function solution(sizes) {
+  const [hor, ver] = sizes.reduce(
+    ([h, v], [a, b]) => [
+      Math.max(h, Math.max(a, b)),
+      Math.max(v, Math.min(a, b)),
+    ],
+    [0, 0]
+  );
+  return hor * ver;
+}
+
+// 다시보기
