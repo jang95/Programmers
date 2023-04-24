df2b3b93f4d168fb723257957a67aa07b698cb38
jang95 <81616561+jang95@users.noreply.github.com>
2023-04-24 18:30:24 +0900

    숫자 문자열과 영단어
    2012 카카오 채용연계형 인턴십

---
+++ src/Level_1/숫자_문자열과_영단어/숫자_문자열과_영단어.js
@@ -0,0 +1,60 @@
+// 나의 풀이
+function solution(s) {
+  if (Number(s)) return Number(s);
+  const num = [
+    'zero',
+    'one',
+    'two',
+    'three',
+    'four',
+    'five',
+    'six',
+    'seven',
+    'eight',
+    'nine',
+  ];
+  for (let i = 0; i < num.length; i++) {
+    s = s.replaceAll(num[i], i);
+  }
+  return Number(s);
+}
+
+// 다른 사람 풀이
+function solution(s) {
+  let numbers = [
+    'zero',
+    'one',
+    'two',
+    'three',
+    'four',
+    'five',
+    'six',
+    'seven',
+    'eight',
+    'nine',
+  ];
+  var answer = s;
+
+  for (let i = 0; i < numbers.length; i++) {
+    let arr = answer.split(numbers[i]);
+    answer = arr.join(i);
+  }
+
+  return Number(answer);
+}
+
+// 정규식 풀이
+function solution(s) {
+  s = s
+    .replace(/zero/gi, 0)
+    .replace(/one/gi, 1)
+    .replace(/two/gi, 2)
+    .replace(/three/gi, 3)
+    .replace(/four/gi, 4)
+    .replace(/five/gi, 5)
+    .replace(/six/gi, 6)
+    .replace(/seven/gi, 7)
+    .replace(/eight/gi, 8)
+    .replace(/nine/gi, 9);
+  return parseInt(s);
+}
