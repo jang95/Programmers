1d30df235d212ffeee0473a6737a1fce9dd3747d
jang95 <81616561+jang95@users.noreply.github.com>
2023-05-12 16:51:20 +0900

    ADD: 코딩 기초 트레이닝

---
+++ 코딩_기초_트레이닝/문자열_출력하기/문자열_출력하기.js
@@ -0,0 +1,14 @@
+// 나의 풀이
+const readline = require('readline');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+let input = [];
+
+rl.on('line', function (line) {
+  input = line;
+}).on('close', function () {
+  console.log(input);
+});
