eda2a092054b046f4d9b222608d4de096442816d
jang95 <ruin950521@naver.com>
2024-08-14 02:52:50 +0900

    Add: 크레인 인형뽑기 게임 - 나의 풀이

---
+++ Level_1/카카오/크레인_인형뽑기_게임/크레인인형뽑기게임.js
@@ -0,0 +1,48 @@
+const Board = [
+  [0, 0, 0, 0, 0],
+  [0, 0, 1, 0, 3],
+  [0, 2, 5, 0, 1],
+  [4, 2, 4, 4, 2],
+  [3, 5, 1, 3, 1],
+];
+
+const Moves = [1, 5, 3, 5, 1, 2, 1, 4];
+
+// 나의 풀이
+function solution(board, moves) {
+  let boardCopy = board.map((item) => [...item]);
+  let basket = [];
+
+  // moves의 크기 만큼 반복해서 인형을 뽑고 그것을 바구니에 담는다.
+  for (let i = 0; i < moves.length; i++) {
+    // 뽑을 위치 계산
+    let line = moves[i] - 1;
+
+    for (let j = 0; j < boardCopy.length; j++) {
+      if (boardCopy[j][line] !== 0) {
+        basket.push(boardCopy[j][line]);
+        // 인형 뽑고 빈칸으로 채움
+        boardCopy[j][line] = 0;
+        break;
+      }
+    }
+  }
+
+  let hasAdjacent = true;
+
+  while (hasAdjacent) {
+    hasAdjacent = false;
+    for (let i = 0; i < basket.length - 1; i++) {
+      if (basket[i] === basket[i + 1]) {
+        // 인접한 숫자가 같으면 배열에서 제거
+        basket.splice(i, 2);
+        // 인형 2개 터짐
+        couter += 2;
+        hasAdjacent = true;
+        break;
+      }
+    }
+  }
+}
+
+solution(Board, Moves);
